8870b1c1f77d459624009d27e081399d45996dd6
Meet Joysar <meetjoysar7s@gmail.com>
2025-11-04 21:51:46 +0530

    modifying index.js again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 const cors = require('cors')
 
 const app = express()
