e4af0496a72f93b8a12cda3e269a9f316e9b76a3
Meet Joysar <meetjoysar7s@gmail.com>
2025-11-08 13:13:23 +0530

    done till ex 3.13

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv').config()
 // const fs = require('fs')
 // const path = require('path')
 // const cors = require('cors')
+
+/*
+1.import mongoos
+2..env file with uri
+3.connect url with mongodb
+4.schema rules
+5.tranforming output
+6.model
+*/
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url).then(result => {
+    console.log('connected to MongoDB');
+}).catch(err => {
+    console.log("error connecting to MongoDB", err.message);
+})
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model("Person", phonebookSchema)
 
 const app = express()
 // app.use(cors())
@@ -22,28 +48,28 @@ app.use(express.json())
 // app.use(morgan('tiny', { stream: accessLogStream }))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let phonebook = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/info', (req, res) => {
     let currentdate = new Date()
@@ -55,7 +81,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(phonebook)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -117,9 +145,13 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
