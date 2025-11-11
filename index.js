e275ad813b0ca475d3f5f1fc7af423ac9deaba94
Meet Joysar <meetjoysar7s@gmail.com>
2025-11-11 13:14:59 +0530

    till ex3.16 error handler separated

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const express = require('express')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 require('dotenv').config()
 const Person = require("./models/phonebook")
+const e = require('express')
 
 const app = express()
 
 app.use(express.static('dist'))
 
-morgan.token('body', (req) => {
-    return JSON.stringify(req.body)
-})
+// morgan.token('body', (req) => {
+//     return JSON.stringify(req.body)
+// })
 
 app.use(express.json())
 // app.use(morgan('tiny', { stream: accessLogStream }))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('----------------------')
+  next()
+}
+app.use(requestLogger)
 
 app.get('/info', (req, res) => {
-    let currentdate = new Date()
-    let datetime = currentdate.toString()
-    // console.log(d.getFullYear());
-    res.send(
-        `<p>Phonebook has info for ${phonebook.length} people.</p><p>${datetime}</p>`
-    )
+    Person.countDocuments({}).then(count => {
+        let datetime = new Date().toString()
+        res.send(
+            `<p>Phonebook has info for ${count} people.</p><p>${datetime}</p>`
+        )
+    })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -35,15 +44,9 @@ app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
     Person.findById(id).then(person => {
         res.json(person)
-    })
+    }).catch(err => next(err))
 })
 
-const generateId = () =>{
-    const maxId = phonebook.length > 0
-        ? Math.max(...phonebook.map(p => Number(p.id)))
-        : 0
-    return String(maxId + 1)
-}
 
 app.post('/api/persons', (req, res) => {
     let name_2put = req.body.name
@@ -71,10 +74,13 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    phonebook = phonebook.filter(person => person.id !== id)
+    // const id = req.params.id
+    // phonebook = phonebook.filter(person => person.id !== id)
 
-    res.status(204).end()
+    // res.status(204).end()
+    Person.findByIdAndDelete(req.params.id).then(result => {
+        res.status(204).end()
+    }).catch(err => next(err))
 })
 
 const unknownEndpoint = (req, res) => {
@@ -82,6 +88,16 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformated id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
